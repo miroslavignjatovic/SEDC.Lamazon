50a1aa22bf04c8a0e4440c1a25f063b1663bfe5c
miroslav <miroslav.ignjatovic@gmail.com>
2023-11-28 19:32:06 +0100

    Add project files.

---
+++ SEDC.Lamazon.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.Lamazon.Domain.Entities;
+
+public class ProductCategory : BaseEntity
+{
+    public string Name { get; set; }
+}
